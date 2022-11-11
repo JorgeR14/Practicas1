4a57c19d5376ea7c02703984b6ca99cd2bc61a9c
JorgeR14 <jrd94jorge@hotmail.com>
2022-11-11 15:50:05 +0100

    retoqueCoins

--- Practica1/Assets/scripts/Coins.cs
+++ Practica1/Assets/scripts/Coins.cs
@@ -6,10 +6,34 @@ public class Coins : PlayerTrigger
 {
     public int amountToGive = 1;
 
+    private AudioSource myAudioSource;
+
+    private MeshRenderer coinMesh;
+    private Collider coinCollider;
+
+    private void Start()
+    {
+        myAudioSource = GetComponentInChildren<AudioSource>();
+        coinMesh = GetComponentInChildren<MeshRenderer>();
+        coinCollider = GetComponentInChildren<Collider>();
+    }
+
     public override void OnPlayerEnter(GameObject playerobject)
     {
         CoinCounter counter = playerobject.GetComponent<CoinCounter>();
         counter.getCoins(amountToGive);
-        gameObject.SetActive(false);
+
+        myAudioSource.Play();
+
+        coinMesh.enabled = false;
+        coinCollider.enabled = false;
+
+        StartCoroutine(DestroyCoinCoroutine());
+    }
+
+    public IEnumerator DestroyCoinCoroutine()
+    {
+        yield return new WaitForSeconds(myAudioSource.clip.length);
+        Destroy(gameObject);
     }
 }
